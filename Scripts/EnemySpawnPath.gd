c34355bdd305c303ec8bddfa9c5862273a0d956f
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-10-03 14:58:26 +0200

    add minimum spawn distance for enemies

--- Scripts/EnemySpawnPath.gd
+++ Scripts/EnemySpawnPath.gd
@@ -1,11 +1,24 @@
 extends Path2D
 
 var enemyScene = load("res://Scenes/Enemy.tscn")
+export var minimumSpawnDistance = 500
+
 
 func _ready():
 	pass
 
+func getNewSpawnPosition():
+	return curve.interpolate_baked(randf() * curve.get_baked_length(), false)
+
 func _on_EnemySpawnTimer_timeout():
 	var enemy = enemyScene.instance()
-	enemy.position = curve.interpolate_baked(randf() * curve.get_baked_length(), false)
+	
+	var attemptedSpawnPosition = getNewSpawnPosition()
+	var player = get_node("../Player")
+	
+	while (player.position - attemptedSpawnPosition).length() < minimumSpawnDistance:
+		attemptedSpawnPosition = getNewSpawnPosition()
+	
+	enemy.position = attemptedSpawnPosition
+	
 	get_parent().add_child(enemy)
