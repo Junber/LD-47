6f72142788c1aad4c9689c0ee9f81d1fc55c3c08
Vincent Opitz <vincent.opitz@student.hpi.de>
2020-10-03 16:48:32 +0200

    add laser to cyborg not working lol

---
+++ Scripts/Laser.gd
@@ -0,0 +1,16 @@
+extends RayCast2D
+
+onready var laserBeam = $LaserBeam
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	rotation += delta
+	
+	if is_colliding():
+		var point = get_collision_point()
+		laserBeam.points[1]=point
+		print(get_collider().name)
