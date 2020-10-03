a030ca4f29bb4b01c6de17ba7f9ffd8f2098f8e9
Julian Berger <julian.berger@student.hpi.de>
2020-10-03 17:03:36 +0200

    "Review"

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -2,11 +2,6 @@ extends RayCast2D
 
 onready var laserBeam = $LaserBeam
 
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	rotation += delta
 	
