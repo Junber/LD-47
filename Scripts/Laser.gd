489dd37b14d435050b1691ec15b2caa8aeb22c11
Vincent <vincent.opitz@student.hpi.de>
2020-10-04 12:30:56 +0200

    Cyborg now telegraphs the laser before it damages

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -4,6 +4,8 @@ onready var laserBeam = $LaserBeam
 onready var laserSparks = $Sparks
 export var damage = -800
 
+var damaging = false
+
 func _process(_delta):
 	if get_parent().dead:
 		queue_free()
@@ -15,5 +17,10 @@ func _physics_process(delta):
 		var laserEndPoint = to_local(get_collision_point())
 		laserBeam.points[1] = laserEndPoint
 		laserSparks.position = laserEndPoint
-	get_collider().collideWithLaser(get_parent(), damage * delta)
+		if damaging:
+			get_collider().collideWithLaser(get_parent(), damage * delta)
 
+func _on_StartupTimer_timeout():
+	damaging = true
+	$LaserBeam.default_color = Color("ffffff")
+	$Sparks.emitting = true
