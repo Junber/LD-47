f790fe4de8fc19935fc48a2d94b2217fbf32f1fa
Vincent <vincent.opitz@student.hpi.de>
2020-10-04 23:04:41 +0200

    add particles to end of combobar fill

--- Scripts/ScoreSystem.gd
+++ Scripts/ScoreSystem.gd
@@ -5,6 +5,10 @@ var currentStreak = 0
 
 export var comboTimeIncreasePerKill = 3
 
+
+func _ready():
+	$ComboBar/DecreasingOfBar.process_material.emission_box_extents.y = $ComboBar.get_progress_texture().get_height()
+
 func showStreak():
 	$ComboBar/StreakLabel.text = "x " + str(currentStreak)
 
@@ -20,15 +24,23 @@ func increaseScore():
 	$ScoreLabel.rect_position += $BigScorePosition.position
 	$ScoreLabel.rect_scale = Vector2(2, 2)
 	$ScoreLabelEffectTimer.start()
+	$ComboBar/DecreasingOfBar.emitting=true
 
 func endStreak():
 	currentStreak = 0
 	showStreak()
 
+func comboBarFillRatio():
+	return $ComboBar.value/$ComboBar.max_value
+
 func _process(delta):
 	$ComboBar.value = $ComboBar.value - delta
 	if $ComboBar.value == 0:
+		$ComboBar/DecreasingOfBar.emitting=false
 		endStreak()
+	
+	$ComboBar/DecreasingOfBar.position = Vector2($ComboBar.get_progress_texture().get_width(),0) * comboBarFillRatio()
+	print($ComboBar/DecreasingOfBar.position)
 	
 	if !$ScoreLabelEffectTimer.is_stopped():
 		$ScoreLabel.set_rotation(rand_range(-0.1, 0.1))
