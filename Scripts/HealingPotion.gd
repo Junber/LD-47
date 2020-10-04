287048ba72e8ea5b727a50286e39df7c7c83cb0f
Vincent <vincent.opitz@student.hpi.de>
2020-10-04 19:56:20 +0200

    add item mega superscene

--- Scripts/HealingPotion.gd
+++ Scripts/HealingPotion.gd
@@ -1,8 +1,6 @@
-extends KinematicBody2D
+extends "res://Scripts/Item.gd"
 
 export var healingAmount = 50
-
-var velocity = Vector2(0,0)
 
 func getHitBy(collider, _collision):
 	collider.collideWithHealingPotion(self)
@@ -12,14 +10,3 @@ func getUsed():
 
 func collideWithWall(_collider, collision):
 	velocity = velocity.bounce(collision.normal) / 2
-
-func _process(delta):
-	velocity *= pow(0.9, delta)
-	velocity -= velocity.normalized() * 100 * delta
-	#warning-ignore:return_value_discarded
-	move_and_collide(velocity * delta)
-
-#this only allows the potion to be picked up when it almost stopped
-#which is a cheap workaround/feature for avoiding it gettng used when spawned
-func _on_NoPickUpTimer_timeout():
-	set_collision_mask_bit(0, true)
---
+++ Scripts/Item.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+
+func getHitBy(collider, _collision):
+	pass
+
+func getUsed():
+	pass
+
+func collideWithWall(_collider, collision):
+	velocity = velocity.bounce(collision.normal) / 2
+
+func _process(delta):
+	velocity *= pow(0.9, delta)
+	velocity -= velocity.normalized() * 100 * delta
+	#warning-ignore:return_value_discarded
+	move_and_collide(velocity * delta)
+
+#this only allows the potion to be picked up when it almost stopped
+#which is a cheap workaround/feature for avoiding it gettng used when spawned
+func _on_NoPickUpTimer_timeout():
+	set_collision_mask_bit(0, true)
