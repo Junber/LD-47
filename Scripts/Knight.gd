6a792beb9fe354fc8ae2254bb869bf797caf8821
Vincent <vincopi@googlemail.com>
2020-10-06 21:02:47 +0200

    Shield rotation speed is adjustable

--- Scripts/Knight.gd
+++ Scripts/Knight.gd
@@ -3,22 +3,29 @@ extends "res://Scripts/Enemy.gd"
 var shieldRotation = 0
 export var shieldedAngle = 180
 
+#this is the angle in radians that the shield rotates per second
+export var shieldRotationSpeed = PI / 2
+
 func kill():
 	if !dead:
 		.kill()
 		$ShieldSprite.visible = false
 
+func angleToShield(theirPosition):
+	return (theirPosition - global_position).angle_to(Vector2(0,-1).rotated(shieldRotation))
+
 func protected(collider):
 	#the shielded angled is halved because the shied extends to both sides
-	return abs((collider.global_position - global_position).angle_to(Vector2(0,-1).rotated(shieldRotation))) <= shieldedAngle * PI / 180 / 2
+	return abs(angleToShield(collider.global_position)) <= shieldedAngle * PI / 180 / 2
 
 func getDirection():
 	return .getDirection() / 2
 
 func _process(delta):
 	if !dead:
-		var angle = ($"../Player".global_position - global_position).angle_to(Vector2(0,-1).rotated(shieldRotation))
-		
+		var angle = angleToShield($"../Player".global_position)
+		print(angle)
+				
 		if abs(angle) > 0.1:
-			shieldRotation += -sign(angle) * delta
+			shieldRotation += -sign(angle) * shieldRotationSpeed * delta
 		$ShieldSprite.rotation = shieldRotation - rotation
